156f98b063e92d8ebcf30c0e58e3eba2ebe46e29
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-05-09 19:43:47 +0545

    finalzing implementtion and adding some jest tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv-safe'
+
 import add from '@src/calculate/add'
 
-console.log(`calculate add: ${add(2, 2)}`)
+dotenv.config()
+
+console.log(`calculate add: ${add(2, 3)}`)
+console.log(`env variable: ${process.env.MY_VARIABLE}`)
